34f75480e6e108070d994d94ee3f547d286b720a
Владимир Ильич <stepanovkost4ntin@yandex.ru>
2026-09-08 11:36:12 +0300

    Создание файла.

---
+++ labs2/File2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+	double a, b, c;
+
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+
+	return 0;
+}
